42799bf2a1da10016437b91cc7d83dbb9d8b5ab4
Sanjay Shah <Sanjay Shah>
2017-10-06 14:06:43 -0700

    Initial Commit

---
+++ PaymentsApp/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PaymentsApp
+//
+//  Created by Sanjay Shah on 2017-10-06.
+//  Copyright © 2017 Sanjay Shah. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
